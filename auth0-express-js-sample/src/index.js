56a35127074b92301566c1587e52030d10f55da8
Daniel Ben Zaken <benzaked@post.bgu.ac.il>
2020-10-25 20:05:06 +0200

    prepare to deploy

--- auth0-express-js-sample/src/index.js
+++ auth0-express-js-sample/src/index.js
@@ -19,7 +19,16 @@ const apiRouter = express.Router();
 /**
  *  App Configuration
  */
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.use(helmet());
 app.use(cors({ origin: clientOrigins }));
 app.use(express.json());
