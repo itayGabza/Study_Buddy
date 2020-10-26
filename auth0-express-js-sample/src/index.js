625ddd847691b299e495316df5185fd60e91d1a9
Daniel Ben Zaken <benzaked@post.bgu.ac.il>
2020-10-26 19:51:34 +0200

    not working

--- auth0-express-js-sample/src/index.js
+++ auth0-express-js-sample/src/index.js
@@ -42,7 +42,14 @@ app.use(express.static(path.join(__dirname, '/../client/build')));
 //production mode
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/../client/build')));
-    app.get('*', (req, res) => { res.sendfile(path.join(__dirname = '/../client/build/index.html')); })
+    app.get('*', (req, res) => { path.join(__dirname = '/../client/build/index.html')
+    , function(err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err); 
+  
+  })
+
 }
 //build mode
 app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/../client/public/index.html'));})
