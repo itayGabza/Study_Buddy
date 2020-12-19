9bbcde126aa6625ca3f2be6219da51bdb04e3520
Daniel Ben Zaken <benzaked@post.bgu.ac.il>
2020-12-19 14:09:47 +0200

    edding filters

--- auth0-express-js-sample/client/src/components/Filters.js
+++ auth0-express-js-sample/client/src/components/Filters.js
@@ -1,173 +1,217 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-// import CheckBoxIcon from '@material-ui/icons/CheckBox';
-// import Favorite from '@material-ui/icons/Favorite';
-// import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import React, { Component } from "react";
+import Select from "react-select";
+import "./Filters.css";
+// import { filter, findWhere,} from "underscore";
+import "bootstrap/dist/css/bootstrap.css";
+const options = [
+  { value: "homeWork", label: "שיעורי בית", column: "1" },
+  { value: "test", label: "למידה למבחן", column: "1" },
+  { value: "other", label: "אחר", column: "1" },
+  { value: "male", label: "גבר", column: "2" },
+  { value: "female", label: "אישה", column: "2" },
+  { value: "mix", label: "תביאו לי הכל מהכל", column: "2" },
+  { value: "good", label: "טוב", column: "3" },
+  { value: "medium", label: "בינוני", column: "3" },
+  { value: "bad", label: "מתקשה", column: "3" },
+  { value: "zoom", label: "זום", column: "4" },
+  { value: "frontal", label: "פנים מול פנים", column: "4" },
+  { value: "morning", label: "בבוקר", column: "5" },
+  { value: "noon", label: "בצהריים", column: "5" },
+  { value: "afterNoon", label: "אחר הצהריים", column: "5" },
+  { value: "Evening", label: "בערב", column: "5" },
+  { value: "2", label: "2", column: "6" },
+  { value: "3", label: "3", column: "6" },
+  { value: "4", label: "4", column: "6" },
+  { value: "5Plus", label: "5+", column: "6" }
+];
 
+class Filters extends Component {
+  constructor(props) {
+    super(props);
+    this.columnsToDisplay = this.columnsToDisplay.bind(this);
 
-import './Filters.css';
+    this.state = {
+      columnsToRender: [true, true, true, true, true, true], // change to amount of columns
+      values: [],
+      buttonsToRender: options
+    };
+  }
 
-const GreenCheckbox = withStyles({
-  root: {
-    color: green[400],
-    '&$checked': {
-      color: green[600],
-    },
-  },
-  checked: {},
-})((props) => <Checkbox color="default" {...props} />);
-
-export default function CheckboxLabels() {
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-    checkedF: true,
-    checkedG: true,
-  });
-
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
+  comparer = (otherArray) => {
+    return function (current) {
+      return (
+        otherArray.filter(function (other) {
+          return other.value === current.value && other.label === current.label;
+        }).length === 0
+      );
+    };
+  };
+  countColumns = (options) => {
+    var columns = {},
+      e;
+    for (var i = 0, l = options.length; i < l; i++) {
+      e = options[i];
+      columns[e.column] = (columns[e.column] || 0) + 1;
+    }
+    return columns;
   };
 
-  return (
-    <FormGroup row>
-      <div >
-        <h6>Objective</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG1} onChange={handleChange} name="checkedG1" />}
-            label="Home Work"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG2} onChange={handleChange} name="checkedG2" />}
-              label="Exam preperation"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG17} onChange={handleChange} name="checkedG17" />}
-              label="Other"
-            />
-          </div>
-        </FormGroup>
-      </div>
-      <div >
-        <h6>Gender</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG1} onChange={handleChange} name="checkedG1" />}
-            label="Male"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG2} onChange={handleChange} name="checkedG2" />}
-              label="Female"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG17} onChange={handleChange} name="checkedG17" />}
-              label="Mix"
-            />
-          </div>
-        </FormGroup>
-      </div>
-      <div >
-        <h6>Level</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG13} onChange={handleChange} name="checkedG13" />}
-            label="Good"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG14} onChange={handleChange} name="checkedG14" />}
-              label="Medium"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG15} onChange={handleChange} name="checkedG15" />}
-              label="Bad"
-            />
-          </div>
-        </FormGroup>
-      </div>
-      <div >
-        <h6>Location</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG11} onChange={handleChange} name="checkedG11" />}
-            label="Zoom"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG12} onChange={handleChange} name="checkedG12" />}
-              label="Frontal"
-            />
-          </div>
-        </FormGroup>
-      </div>
+  columnsToDisplay = (options, buttonsToRender) => {
+    var countOptions = this.countColumns(options);
+    var countButtonToRender = this.countColumns(buttonsToRender);
+    var columnsToRenderLocal = [];
+    var a, b;
+    for (var i = 1, l = Object.keys(countOptions).length; i <= l; i++) {
+      a = countOptions[i];
+      b = countButtonToRender[i];
+      if (a !== b) {
+        columnsToRenderLocal.push(false);
+      } else {
+        columnsToRenderLocal.push(true);
+      }
+    }
+    console.log("cols", columnsToRenderLocal);
+    this.setState({
+      columnsToRender: columnsToRenderLocal
+    });
+  };
+
+  handleChange = (values) => {
+    this.setState({
+      values: values,
+      buttonsToRender: options.filter(this.comparer(values))
+    });
+    this.columnsToDisplay(options, options.filter(this.comparer(values)));
+  };
+
+  addToList = (value) => {
+    this.setState({
+      values: [...this.state.values, value]
+    });
+    this.handleChange([...this.state.values, value]);
+  };
+  render() {
+    const { values } = this.state;
+    return (
       <div>
-        <h6>Studying time</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG3} onChange={handleChange} name="checkedG3" />}
-            label="Morning"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG4} onChange={handleChange} name="checkedG4" />}
-              label="Noon"
-            />
+        <Select
+          hideSelectedOptions={true}
+          isMulti
+          options={options}
+          onChange={this.handleChange}
+          value={values}
+          isSearchable={false}
+          openMenuOnClick={false}
+          menuIsOpen={false}
+        />
+        {/* <div className="selected-options">
+          {values.length > 0 && values.map((item) => <p>{item.label}</p>)}
+        </div> */}
+
+        <div class="container" className="gridDesign">
+          <div class="row">
+            {this.state.columnsToRender[0] === true ? (
+              <div class="col-sm-2">
+                <a>{"סוג"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "1" ? (
+                    <div class="mb-3">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
+            {this.state.columnsToRender[1] === true ? (
+              <div class="col-sm-2">
+                <a>{"מגדר"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "2" ? (
+                    <div class="mb-3">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
+            {this.state.columnsToRender[2] === true ? (
+              <div class="col-sm-2">
+                <a>{"רמת שליטה בחומר"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "3" ? (
+                    <div class="mb-3">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
+            {this.state.columnsToRender[3] === true ? (
+              <div class="col-sm-2">
+                <a>{"סביבת הלמידה"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "4" ? (
+                    <div class="mb-3">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
+            {this.state.columnsToRender[4] === true ? (
+              <div class="col-sm-2">
+                <a>{"זמן מועדף ללמידה"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "5" ? (
+                    <div class="mb-3">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
+            {this.state.columnsToRender[5] === true ? (
+              <div class="col-sm-2">
+                <a>{"כמות שותפים"}</a>
+                {this.state.buttonsToRender.map((item, i) =>
+                  item.column === "6" ? (
+                    <div class="mb-2">
+                      <button href="#" onClick={() => this.addToList(item)}>
+                        {item.label}
+                      </button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )
+                )}
+              </div>
+            ) : null}
           </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG5} onChange={handleChange} name="checkedG5" />}
-              label="Afternoon"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG6} onChange={handleChange} name="checkedG6" />}
-              label="Evening"
-            />
-          </div>
-        </FormGroup>
+        </div>
+        <br />
       </div>
-      <div>
-        <h6>Group size</h6>
-        <FormGroup col>
-          <FormControlLabel
-            control={<GreenCheckbox checked={state.checkedG7} onChange={handleChange} name="checkedG7" />}
-            label="2"
-          />
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG8} onChange={handleChange} name="checkedG8" />}
-              label="3"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG9} onChange={handleChange} name="checkedG9" />}
-              label="4"
-            />
-          </div>
-          <div class="formgroup">
-            <FormControlLabel
-              control={<GreenCheckbox checked={state.checkedG10} onChange={handleChange} name="checkedG10" />}
-              label="5+"
-            />
-          </div>
-        </FormGroup>
-      </div>
-    </FormGroup>
-  );
+    );
+  }
 }
+
+export default Filters;
